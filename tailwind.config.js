c4f32851ab91f8a63af942cb4a405e4708e69015
kayoooon1 <shiaky3821@gmail.com>
2025-10-02 22:23:00 +0200

    update tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./{app,components,libs,pages,hooks}/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        backgroundCream: '#FAF9F6',
+        textGrayCustom: '#4A4A4A',
+      },
+    },
   },
   plugins: [],
 }
